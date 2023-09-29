b5c872421a8110f807f3a8c3779a73a3952ba6c3
Madonza.RicsonJay <ricsonjaymadonza@gmail.com>
2023-09-29 21:21:48 +0800

    Add project files.

---
+++ StudentGradeApplication/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace StudentGradeApplication
+{
+    partial class StudentGradeApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudentGradeApplication));
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_grades = new System.Windows.Forms.Label();
+            this.lbl_english = new System.Windows.Forms.Label();
+            this.lbl_math = new System.Windows.Forms.Label();
+            this.lbl_science = new System.Windows.Forms.Label();
+            this.lbl_filipino = new System.Windows.Forms.Label();
+            this.lbl_history = new System.Windows.Forms.Label();
+            this.tbx_name = new System.Windows.Forms.TextBox();
+            this.tbx_english = new System.Windows.Forms.TextBox();
+            this.tbx_math = new System.Windows.Forms.TextBox();
+            this.tbx_science = new System.Windows.Forms.TextBox();
+            this.tbx_filipino = new System.Windows.Forms.TextBox();
+            this.tbx_history = new System.Windows.Forms.TextBox();
+            this.btn_average = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.tbx_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name.Location = new System.Drawing.Point(77, 49);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(56, 19);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "Name :";
+            // 
+            // lbl_grades
+            // 
+            this.lbl_grades.AutoSize = true;
+            this.lbl_grades.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_grades.Location = new System.Drawing.Point(230, 112);
+            this.lbl_grades.Name = "lbl_grades";
+            this.lbl_grades.Size = new System.Drawing.Size(55, 19);
+            this.lbl_grades.TabIndex = 1;
+            this.lbl_grades.Text = "Grades";
+            // 
+            // lbl_english
+            // 
+            this.lbl_english.AutoSize = true;
+            this.lbl_english.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_english.Location = new System.Drawing.Point(79, 152);
+            this.lbl_english.Name = "lbl_english";
+            this.lbl_english.Size = new System.Drawing.Size(56, 19);
+            this.lbl_english.TabIndex = 2;
+            this.lbl_english.Text = "English";
+            // 
+            // lbl_math
+            // 
+            this.lbl_math.AutoSize = true;
+            this.lbl_math.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_math.Location = new System.Drawing.Point(77, 191);
+            this.lbl_math.Name = "lbl_math";
+            this.lbl_math.Size = new System.Drawing.Size(43, 19);
+            this.lbl_math.TabIndex = 3;
+            this.lbl_math.Text = "Math";
+            // 
+            // lbl_science
+            // 
+            this.lbl_science.AutoSize = true;
+            this.lbl_science.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_science.Location = new System.Drawing.Point(77, 229);
+            this.lbl_science.Name = "lbl_science";
+            this.lbl_science.Size = new System.Drawing.Size(59, 19);
+            this.lbl_science.TabIndex = 4;
+            this.lbl_science.Text = "Science";
+            // 
+            // lbl_filipino
+            // 
+            this.lbl_filipino.AutoSize = true;
+            this.lbl_filipino.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_filipino.Location = new System.Drawing.Point(77, 268);
+            this.lbl_filipino.Name = "lbl_filipino";
+            this.lbl_filipino.Size = new System.Drawing.Size(58, 19);
+            this.lbl_filipino.TabIndex = 5;
+            this.lbl_filipino.Text = "Filipino";
+            // 
+            // lbl_history
+            // 
+            this.lbl_history.AutoSize = true;
+            this.lbl_history.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_history.Location = new System.Drawing.Point(77, 308);
+            this.lbl_history.Name = "lbl_history";
+            this.lbl_history.Size = new System.Drawing.Size(56, 19);
+            this.lbl_history.TabIndex = 6;
+            this.lbl_history.Text = "History";
+            // 
+            // tbx_name
+            // 
+            this.tbx_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_name.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_name.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_name.Location = new System.Drawing.Point(147, 46);
+            this.tbx_name.Name = "tbx_name";
+            this.tbx_name.Size = new System.Drawing.Size(563, 27);
+            this.tbx_name.TabIndex = 7;
+            this.tbx_name.UseWaitCursor = true;
+            this.tbx_name.TextChanged += new System.EventHandler(this.tbx_name_TextChanged);
+            // 
+            // tbx_english
+            // 
+            this.tbx_english.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_english.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_english.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_english.Location = new System.Drawing.Point(147, 149);
+            this.tbx_english.Name = "tbx_english";
+            this.tbx_english.Size = new System.Drawing.Size(234, 27);
+            this.tbx_english.TabIndex = 8;
+            this.tbx_english.TextChanged += new System.EventHandler(this.tbx_english_TextChanged);
+            // 
+            // tbx_math
+            // 
+            this.tbx_math.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_math.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_math.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_math.Location = new System.Drawing.Point(147, 188);
+            this.tbx_math.Name = "tbx_math";
+            this.tbx_math.Size = new System.Drawing.Size(234, 27);
+            this.tbx_math.TabIndex = 9;
+            this.tbx_math.TextChanged += new System.EventHandler(this.tbx_math_TextChanged);
+            // 
+            // tbx_science
+            // 
+            this.tbx_science.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_science.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_science.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_science.Location = new System.Drawing.Point(147, 226);
+            this.tbx_science.Name = "tbx_science";
+            this.tbx_science.Size = new System.Drawing.Size(234, 27);
+            this.tbx_science.TabIndex = 10;
+            this.tbx_science.TextChanged += new System.EventHandler(this.tbx_science_TextChanged);
+            // 
+            // tbx_filipino
+            // 
+            this.tbx_filipino.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_filipino.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_filipino.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_filipino.Location = new System.Drawing.Point(147, 265);
+            this.tbx_filipino.Name = "tbx_filipino";
+            this.tbx_filipino.Size = new System.Drawing.Size(234, 27);
+            this.tbx_filipino.TabIndex = 11;
+            // 
+            // tbx_history
+            // 
+            this.tbx_history.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.tbx_history.Font = new System.Drawing.Font("Vecoly", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_history.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbx_history.Location = new System.Drawing.Point(147, 305);
+            this.tbx_history.Name = "tbx_history";
+            this.tbx_history.Size = new System.Drawing.Size(234, 27);
+            this.tbx_history.TabIndex = 12;
+            // 
+            // btn_average
+            // 
+            this.btn_average.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_average.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_average.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_average.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_average.Location = new System.Drawing.Point(464, 149);
+            this.btn_average.Name = "btn_average";
+            this.btn_average.Size = new System.Drawing.Size(184, 54);
+            this.btn_average.TabIndex = 13;
+            this.btn_average.Text = "Calculate";
+            this.btn_average.UseVisualStyleBackColor = false;
+            this.btn_average.Click += new System.EventHandler(this.btn_average_Click);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_result.ForeColor = System.Drawing.Color.Yellow;
+            this.lbl_result.Location = new System.Drawing.Point(536, 244);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(53, 21);
+            this.lbl_result.TabIndex = 15;
+            this.lbl_result.Text = "Result";
+            this.lbl_result.Visible = false;
+            this.lbl_result.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbx_result
+            // 
+            this.tbx_result.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(33)))), ((int)(((byte)(35)))));
+            this.tbx_result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbx_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_result.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tbx_result.Location = new System.Drawing.Point(413, 280);
+            this.tbx_result.Margin = new System.Windows.Forms.Padding(0, 50, 0, 0);
+            this.tbx_result.Multiline = true;
+            this.tbx_result.Name = "tbx_result";
+            this.tbx_result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbx_result.Size = new System.Drawing.Size(297, 75);
+            this.tbx_result.TabIndex = 16;
+            this.tbx_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbx_result.Visible = false;
+            this.tbx_result.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // StudentGradeApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(33)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(763, 385);
+            this.Controls.Add(this.tbx_result);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.btn_average);
+            this.Controls.Add(this.tbx_history);
+            this.Controls.Add(this.tbx_filipino);
+            this.Controls.Add(this.tbx_science);
+            this.Controls.Add(this.tbx_math);
+            this.Controls.Add(this.tbx_english);
+            this.Controls.Add(this.tbx_name);
+            this.Controls.Add(this.lbl_history);
+            this.Controls.Add(this.lbl_filipino);
+            this.Controls.Add(this.lbl_science);
+            this.Controls.Add(this.lbl_math);
+            this.Controls.Add(this.lbl_english);
+            this.Controls.Add(this.lbl_grades);
+            this.Controls.Add(this.lbl_name);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StudentGradeApplication";
+            this.Text = "Student Grade Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_grades;
+        private System.Windows.Forms.Label lbl_english;
+        private System.Windows.Forms.Label lbl_math;
+        private System.Windows.Forms.Label lbl_science;
+        private System.Windows.Forms.Label lbl_filipino;
+        private System.Windows.Forms.Label lbl_history;
+        private System.Windows.Forms.TextBox tbx_name;
+        private System.Windows.Forms.TextBox tbx_english;
+        private System.Windows.Forms.TextBox tbx_math;
+        private System.Windows.Forms.TextBox tbx_science;
+        private System.Windows.Forms.TextBox tbx_filipino;
+        private System.Windows.Forms.TextBox tbx_history;
+        private System.Windows.Forms.Button btn_average;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.TextBox tbx_result;
+    }
+}
+
---
+++ StudentGradeApplication/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class StudentGradeApplication : Form
+    {
+        private readonly object multilineTextBox;
+        //String ric = "ricson";
+
+        public StudentGradeApplication()
+        {
+            InitializeComponent();
+            
+        }
+        //String ric = "ricson";
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void btn_average_Click(object sender, EventArgs e)
+        {
+            
+            Font font = new Font("Microsoft Tai Le", 12f);
+            Font font2 = new Font("Vecoly", 10f);
+
+            float English = float.Parse( tbx_english.Text);
+            float Math = float.Parse(tbx_math.Text);
+            float Science = float.Parse(tbx_science.Text);
+            float Filipino = float.Parse(tbx_filipino.Text);
+            float History = float.Parse(tbx_history.Text);
+
+            float average = (English + Math + Science + Filipino + History) /5;
+            Console.WriteLine(average.ToString("0.00"));
+
+            
+
+
+            if (average > 75.00){
+
+                tbx_result.Visible = true;
+                lbl_result.Visible = true;
+
+                tbx_result.Text = "\n Conratulation " + Clipboard.GetText() + ", you passed! \n" +
+                    "Your general average is " + average + ".";
+                tbx_result.Font = font;
+
+            }else if (average > 100){
+                tbx_result.Visible = true;
+                tbx_result.Text = "Something's wrong";
+                tbx_result.Font = font;
+                lbl_result.Visible = true;
+
+            }else if (average < 75){
+                tbx_result.Visible = true;
+                tbx_result.Text = "\n Unfortunately " + Clipboard.GetText() + ", you did not passed. \n" +
+                    "Your general average is only " + average + ".";
+                tbx_result.Font = font;
+                lbl_result.ForeColor = Color.Red;
+                lbl_result.Visible = true;
+            }
+
+
+
+
+        }
+
+        private void tbx_name_TextChanged(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbx_name.Text);
+            
+        }
+
+        private void tbx_english_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbx_math_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbx_science_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
